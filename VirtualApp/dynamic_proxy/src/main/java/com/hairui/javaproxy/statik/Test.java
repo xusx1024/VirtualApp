bb2e60045191cc2955eaef3a1031a77a51997a61
xushengxing <xusx1024@163.com>
2019-03-15 18:26:54 +0800

    ActivityThread UML图

--- VirtualApp/dynamic_proxy/src/main/java/com/hairui/javaproxy/statik/Test.java
+++ VirtualApp/dynamic_proxy/src/main/java/com/hairui/javaproxy/statik/Test.java
@@ -1,6 +1,7 @@
 package com.hairui.javaproxy.statik;
 
 import com.hairui.javaproxy.Shopping;
+import java.util.HashMap;
 
 /**
  * Fun:
@@ -8,6 +9,8 @@ import com.hairui.javaproxy.Shopping;
  * @author sxx.xu@shunwang.com on 3/15/2019.
  */
 public class Test {
+
+
   public static void main(String[] args) {
     Shopping shopping = new ShoppingImpl();
     shopping.doShopping(100);
@@ -15,4 +18,5 @@ public class Test {
     shopping = new ProxyShopping(shopping);
     shopping.doShopping(100);
   }
+
 }
